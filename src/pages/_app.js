6603cac49947998bf58d881932de311e56caf035
Zheng Zhou <30392089+zhoudaxia233@users.noreply.github.com>
2025-01-14 19:09:57 +0100

    add dark mode

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import "@/styles/globals.css";
+import { ThemeProvider } from 'next-themes'
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
+
+export default MyApp
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import HardDriveAnalysis from '../components/HardDriveAnalysis'
 import MemoryOptions from '../components/MemoryOptions'
+import ThemeToggle from '../components/ThemeToggle'
 
 export default function Home() {
   const [searchQuery, setSearchQuery] = useState('')
@@ -10,18 +11,21 @@ export default function Home() {
       {/* Sticky Search Bar */}
       <div className="sticky top-0 z-10 bg-white dark:bg-black border-b border-black/[.08] dark:border-white/[.145] p-4">
         <div className="max-w-5xl mx-auto">
-          <div className="relative">
-            <input
-              type="text"
-              placeholder="Search software or memory configurations..."
-              value={searchQuery}
-              onChange={e => setSearchQuery(e.target.value)}
-              className="w-full px-4 py-2 rounded-lg border border-black/[.08] dark:border-white/[.145] 
-                bg-gray-50 dark:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            <div className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400">
-              ⌘K
+          <div className="flex items-center gap-4">
+            <div className="relative flex-grow">
+              <input
+                type="text"
+                placeholder="Search software or memory configurations..."
+                value={searchQuery}
+                onChange={e => setSearchQuery(e.target.value)}
+                className="w-full px-4 py-2 rounded-lg border border-black/[.08] dark:border-white/[.145] 
+                  bg-gray-50 dark:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              <div className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400">
+                ⌘K
+              </div>
             </div>
+            <ThemeToggle />
           </div>
         </div>
       </div>
