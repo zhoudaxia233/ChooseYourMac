ecd2d9ee46509792e2a3419a8eb83f7bb47c01b1
Zheng Zhou <30392089+zhoudaxia233@users.noreply.github.com>
2025-01-18 23:39:43 +0100

    feat: integrate next-sitemap for SEO optimization

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://chooseyourmac.info', // Replace with your site's URL
+  generateRobotsTxt: true, // (optional) Generate a robots.txt file
+  // ...other options
+}
