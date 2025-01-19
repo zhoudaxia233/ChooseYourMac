92fe8bdbf4b0a051a93bfd337055b0faeb386e71
Zheng Zhou <30392089+zhoudaxia233@users.noreply.github.com>
2025-01-19 12:04:16 +0100

    feat: add structured data schema for website to improve SEO

--- pages/_app.js
+++ pages/_app.js
@@ -15,12 +15,26 @@ function getInitialLocale() {
 }
 
 function MyApp({ Component, pageProps }) {
+  // Base website schema
+  const websiteSchema = {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    name: 'Choose Your Mac',
+    url: 'https://chooseyourmac.info',
+    description:
+      'The easiest MacBook buying guide for budget-conscious users. Find your perfect MacBook without overspending with our intuitive tools for storage and memory performance.',
+  }
+
   return (
     <>
       <Head>
         <meta
           name="google-site-verification"
           content="SxusvZ7BGYRi-rn1CqZyVEouwZ9hUkjxfPa2K_u8gx0"
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
         />
       </Head>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
